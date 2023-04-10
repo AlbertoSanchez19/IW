225da68e19341722e234022da212ba4f48b94df6
AlbertoSanchez19 <albers19@ucm.es>
2023-04-10 18:13:23 +0200

    Expulsar y bloquear

--- src/main/java/es/ucm/fdi/iw/repository/ProfesorRepository.java
+++ src/main/java/es/ucm/fdi/iw/repository/ProfesorRepository.java
@@ -1,4 +1,5 @@
 package es.ucm.fdi.iw.repository;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -8,10 +9,13 @@ import org.springframework.stereotype.Repository;
 
 import es.ucm.fdi.iw.model.Cuestionario;
 import es.ucm.fdi.iw.model.User;
+
 @Repository
-public interface ProfesorRepository extends JpaRepository <User, Long> {
+public interface ProfesorRepository extends JpaRepository<User, Long> {
 	public Optional<User> findById(long id);
+
 	public List<User> findByRolesContaining(String role);
+
 	public void deleteById(Long id);
 
 	@Query("SELECT u FROM User u WHERE NOT u.roles LIKE '%PROFESOR%'")
